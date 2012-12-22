d036a2cb74adee37c6829197f7d333186cdc11aa
Dominic Wirkner <dominic.wirkner@udo.edu>
2012-12-22 21:38:51 +0100

    Datei vergessen, nochmal commited

---
+++ google-contacts-connector/google-connector/src/Main.java
@@ -0,0 +1,17 @@
+import dli_contacts.google_connector.Connector;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		Connector gc = new Connector();
+		gc.test();
+	}
+
+}
