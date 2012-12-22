b28eaf94fbb94b7ac517b2e9bec164e9f1412f86
Suphis <thorben.seeland@gmail.com>
2012-12-22 01:19:31 +0100

    GoogleConnector-Projekt erstellt (mit GWT) ohne Libraries

---
+++ google-contacts-connector/DLI-GoogleContactsConnector/src/dli_googleContactsConnector/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package dli_googleContactsConnector.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
