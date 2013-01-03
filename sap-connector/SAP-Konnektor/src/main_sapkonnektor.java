cc9d03e03b63b5da519dc782787114c0e8f8e8f8
JimStacey <markus.marzotko@uni-dortmund.de>
2013-01-04 00:11:16 +0100

    so, erste große änderung

--- sap-connector/SAP-Konnektor/src/main_sapkonnektor.java
+++ sap-connector/SAP-Konnektor/src/main_sapkonnektor.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -20,11 +22,24 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 
+import javax.xml.ws.Binding;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.WebServiceFeature;
+
+
+import sap_not.myWebServiceFeature;
+
+import com.sap.xi.appl.se.global.*;
+import com.sap.xi.appl.se.global.SupplierSimpleByNameAndAddressQueryMessageSync.SupplierSimpleSelectionByNameAndAddress;
+
+import dli_contacts.Contact;
+
 import com.sap.xi.appl.se.global.SupplierSimpleByNameAndAddressQueryMessageSync;
 import com.sap.xi.appl.se.global.ServiceECCSUPPLIERSNAQRDEFAULTPROFILE;
 
 
-@WebServiceClient(wsdlLocation="../../wsdl_source/supplier.xml")
+
+
 public class main_sapkonnektor {
 
 	/**
@@ -32,11 +47,51 @@ public class main_sapkonnektor {
 	 */
 		public static void main(String argv[]) {
 		
-		SupplierSimpleByNameAndAddressQueryMessageSync suppquery = new SupplierSimpleByNameAndAddressQueryMessageSync();
+		SupplierSimpleByNameAndAddressQueryMessageSync suppaquery = new SupplierSimpleByNameAndAddressQueryMessageSync();
 		
-		ServiceECCSUPPLIERSNAQRDEFAULTPROFILE service = new ServiceECCSUPPLIERSNAQRDEFAULTPROFILE();
+		ServiceECCSUPPLIERSNAQRDEFAULTPROFILE servic = new ServiceECCSUPPLIERSNAQRDEFAULTPROFILE();
+		
+		//Kommentar
+		SupplierSimpleSelectionByNameAndAddress x = new SupplierSimpleSelectionByNameAndAddress();
+		
+		WebServiceFeature ws = new myWebServiceFeature();
+		
+//		BindingProvider bs = (BindingProvider) ws;
+		
+//		Binding ha = bs.getBinding();
+		
+		
+		//Testcode
+		  SupplierSimpleByNameAndAddressQueryMessageSync suppquery = new SupplierSimpleByNameAndAddressQueryMessageSync();
+		  SupplierSimpleSelectionByNameAndAddress supSelection = new SupplierSimpleSelectionByNameAndAddress();
+		//  supSelection.setXXX();
+		  suppquery.setSupplierSimpleSelectionByNameAndAddress(supSelection);
+		  
+		  SupplierSimpleByNameAndAddressResponseMessageSync result = null;
+		  
+		  ServiceECCSUPPLIERSNAQRDEFAULTPROFILE service = new ServiceECCSUPPLIERSNAQRDEFAULTPROFILE();
+		  
+		  try {
+		   SupplierSimpleByNameAndAddressQueryResponseIn binding = service.getBindingTHTTPAHTTPECCSUPPLIERSNAQRDEFAULTPROFILE();
+		   BindingProvider bp = (BindingProvider) binding;
+		   Map<String, Object> reqCont = bp.getRequestContext();
+		   result = binding.supplierSimpleByNameAndAddressQueryResponseIn(suppquery);
+		  } catch (StandardMessageFault e) {
+		   // TODO Auto-generated catch block
+		   e.printStackTrace();
+		  }
+		  result.getSupplier().get(0).getBasicData().getCommon().getName();
 		
 		
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		Contact xta = new Contact();
 		}
 }
