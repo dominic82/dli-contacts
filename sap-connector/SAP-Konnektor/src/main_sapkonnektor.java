a318c667178f3dbaf5d7868f82916e5f10e8e0a5
JimStacey <markus.marzotko@uni-dortmund.de>
2013-01-05 19:07:50 +0100

    Methode mit Rückgabewert gebaut, Code reinkopiert

--- sap-connector/SAP-Konnektor/src/main_sapkonnektor.java
+++ sap-connector/SAP-Konnektor/src/main_sapkonnektor.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import javax.xml.ws.BindingProvider;
 import javax.xml.ws.soap.SOAPFaultException;
 
@@ -76,4 +78,71 @@ public class main_sapkonnektor {
 
 		Contact xta = new Contact();
 	}
+	
+	public static  List<Contact> fetchContact(Contact filter){
+		
+		// BindingProvider bs = (BindingProvider) ws;
+
+		// Binding ha = bs.getBinding();
+
+		// Testcode
+		SupplierSimpleByNameAndAddressQueryMessageSync suppquery = new SupplierSimpleByNameAndAddressQueryMessageSync();
+		SupplierSimpleSelectionByNameAndAddress supSelection = new SupplierSimpleSelectionByNameAndAddress();
+
+		// supSelection.setXXX();
+		// Hier die Werte des zu sendenden Objekts mit den Werten von Dominiks
+		// Contacts befüllen
+		// supSelection.setSupplierName1("");
+		// supSelection.setSupplierName2("Sa");
+		supSelection.setSupplierAddressCountryCode("DE");
+		suppquery.setSupplierSimpleSelectionByNameAndAddress(supSelection);
+
+		SupplierSimpleByNameAndAddressResponseMessageSync result = null;
+
+		ServiceECCSUPPLIERSNAQRDEFAULTPROFILE service = new ServiceECCSUPPLIERSNAQRDEFAULTPROFILE();
+
+		SupplierSimpleByNameAndAddressQueryResponseIn binding = service
+				.getBindingTHTTPAHTTPECCSUPPLIERSNAQRDEFAULTPROFILE();
+		BindingProvider bp = (BindingProvider) binding;
+		// Map<String, Object> reqCont = bp.getRequestContext();
+
+		// bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+		// "Webaddresse");
+		/*
+		 * bp.getRequestContext()
+		 * .put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+		 * "http://erp.esworkplace.sap.com/sap/bc/srt/pm/sap/ecc_supplierbasicdatabyidqr/800/default_profile/2/binding_t_http_a_http_ecc_supplierbasicdatabyidqr_default_profile"
+		 * );
+		 */
+		bp.getRequestContext().put(BindingProvider.USERNAME_PROPERTY,
+				"S0008266219");
+		bp.getRequestContext().put(BindingProvider.PASSWORD_PROPERTY,
+				"Fleischgans85");
+
+		try {
+			result = binding
+					.supplierSimpleByNameAndAddressQueryResponseIn(suppquery);
+		} catch (StandardMessageFault e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SOAPFaultException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// result.getSupplier().get(0).getBasicData().getCommon().getName();
+
+		result.getSupplier().get(5).getBasicData().getCommon().getName()
+				.getFirstLineName();
+
+		System.out.println(result.getSupplier().get(8).getBasicData()
+				.getCommon().getName().getFirstLineName());
+
+		Contact xta = new Contact();
+		
+		List<Contact> Kontaktliste = null;
+		
+		return Kontaktliste;
+		}	
+	
 }
