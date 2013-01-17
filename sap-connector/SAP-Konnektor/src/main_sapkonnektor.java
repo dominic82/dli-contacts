c3171abd0587823649977b1a63d9510dcd3d97e4
JimStacey <markus.marzotko@uni-dortmund.de>
2013-01-17 23:40:34 +0100

    letzte

--- sap-connector/SAP-Konnektor/src/main_sapkonnektor.java
+++ sap-connector/SAP-Konnektor/src/main_sapkonnektor.java
@@ -57,13 +57,14 @@ public class main_sapkonnektor {
 		
 		List<Contact> lk = new LinkedList<Contact>();
 		
-		tk.setType(Contact.ContactType.CUSTOMER);
+		tk.setType(Contact.ContactType.SUPPLIER);
 		
-		tk.setFirstname("Anja");
+		
 		
 		lk = fetchContact(tk);
 		if(lk.size()>0){
 		System.out.println(lk.get(0).getFirstname());
+		System.out.println(lk.get(0).getLastname());
 		System.out.println(lk.get(0).getCity());
 		System.out.println(lk.get(0).getZipcode());
 		System.out.println(lk.get(0).getCompany());
